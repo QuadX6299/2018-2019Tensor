18fd903a7112b34287dfd8490b4c70825bc3a29b
idevneo <idevneo@gmail.com>
2019-01-18 17:22:22 -0600

    tensor flow changes so we don't have to copy in each program

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TensorFlowMainAuto.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TensorFlowMainAuto.java
@@ -47,13 +47,6 @@ import java.util.List;
 
 public class TensorFlowMainAuto extends MyOpModeNEW
 {
-    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
-    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
-    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
-    private static final String VUFORIA_KEY = "AXb/g5n/////AAAAGSUed2rh5Us1jESA1cUn5r5KDUqTfwO2woh7MxjiLKSUyDslqBAgwCi0Qmc6lVczErnF5TIw7vG5R4TJ2igvrDVp+dP+3i2o7UUCRRj/PtyVgb4ZfNrDzHE80/6TUHifpKu4QCM04eRWYZocWNWhuRfytVeWy6NSTWefM9xadqG8FFrFk3XnvqDvk/6ZAgerNBdq5SsJ90eDdoAhgYEee40WxasoUUM9YVMvkWOqZgHSuraV2IyIUjkW/u0O+EkFtTNRUWP+aZwn1qO1H4Lk07AJYe21eqioBLMdzY7A8YqR1TeQ//0WJg8SFdXjuGbF6uHykBe2FF5UeyaehA0iTqfPS+59FLm8y1TuUt57eImq";
-    private VuforiaLocalizer vuforia;
-    private TFObjectDetector tfod;
-
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initialized");
@@ -135,45 +128,7 @@ public class TensorFlowMainAuto extends MyOpModeNEW
         Thread.sleep(1000);
 
         while (time.milliseconds() < 15000) {
-            if (tfod != null) {
-                // getUpdatedRecognitions() will return null if no new information is available since
-                // the last time that call was made.
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    if (updatedRecognitions.size() == 2) {
-                        int goldMineralX = -1;
-                        int silverMineral1X = -1;
-                        int silverMineral2X = -1;
-                        for (Recognition recognition : updatedRecognitions) {
-                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                                Thread.sleep(750);
-                                goldMineralX = (int) recognition.getLeft();
-                            } else if (silverMineral1X == -1) {
-                                Thread.sleep(750);
-                                silverMineral1X = (int) recognition.getLeft();
-                            } else {
-                                Thread.sleep(750);
-                                silverMineral2X = (int) recognition.getLeft();
-                            }
-                        }
-                        if (goldMineralX == -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                            position = "Left";
-                            telemetry.addData("Gold Mineral Position", "Left");
-                        } else if (goldMineralX != 1 && silverMineral1X != -1) {
-                            if (goldMineralX > silverMineral1X) {
-                                position = "Right";
-                                telemetry.addData("Gold Mineral Position", "Right");
-                            } else {
-                                position = "Center";
-                                telemetry.addData("Gold Mineral Position", "Center");
-
-                            }
-                        }
-                    }
-                    telemetry.update();
-                }
-            }
+            sample();
         }
 
 
@@ -215,28 +170,5 @@ public class TensorFlowMainAuto extends MyOpModeNEW
             }
 
             tfod.shutdown();
-    }
-
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = CameraDirection.BACK;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
-    }
-
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
     }
 }
